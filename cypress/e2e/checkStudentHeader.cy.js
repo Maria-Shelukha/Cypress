2b598b379aefb7f5d88f00bc92e09eb25ea4d3d2
[mari.shelukha@gmail.com] <maria.shelukha@nimbusweb.me>
2024-06-09 22:51:48 +0300

    HW 5.3.1 added new custom command

--- cypress/e2e/checkStudentHeader.cy.js
+++ cypress/e2e/checkStudentHeader.cy.js
@@ -8,60 +8,68 @@ describe('verifier site', () => {
    
 
   it('User can follow the header links', () => {
-    
-    
     //1. Avaliable more header links for authorised user
     cy.get('[data-cy=navbar] li').should('have.length', 5)
-    
+  })
+  
+  it('Moving to the Task item on Entity dropdown', () => {
     //2. Moving to the Task item on Entity dropdown
     cy.get('[data-cy=entity]').click()
     cy.get('.dropdown-item').eq(0).should('have.attr', 'href', '/task').click()
     cy.url().should('include', '/task?page=1&sort=id,asc')
     cy.get('.dropdown-item').eq(0).should('have.class', 'active')
+  })
 
+  it('Moving to the User Task item on Entity dropdown', () => {
     //3. Moving to the User Task item on Entity dropdown
     cy.get('[data-cy=entity]').click()
     cy.get('.dropdown-item').eq(1).should('have.attr', 'href', '/user-task').click()
     cy.url().should('include', '/user-task')
     cy.get('.dropdown-item').eq(1).should('have.class', 'active')
+  })
 
+  it('Check home link', () => {
     //4. Check home link
+    cy.get('[data-cy=entity]').click()
+    cy.get('.dropdown-item').eq(1).should('have.attr', 'href', '/user-task').click()
     cy.url().should('include', '/user-task')
     cy.get('.nav-link').contains('Home').click()
     cy.url().should('include', '/?page=1&sort=id,asc')
     cy.get('.nav-link').should('have.class', 'active')
+  })
 
+  it('Moving to API item on Swagger dropdownk', () => {
     //5. Moving to API item on Swagger dropdown
     cy.get('[data-cy=docsMenu]').should('be.visible').click()
     cy.get('.dropdown-item').contains('API').click()
     cy.url().should('include', '/docs/docs')
+  })
 
-    //6. Click on Français on the language menu
-    cy.get('.nav-link').contains('English').click()
-    cy.get('button').contains('Français').click().wait(1000)
-    cy.get('.nav-link').eq(0).should('have.text', 'Accueil')
+  it('Click on Français on the language menu', () => {
+    cy.switchLanguage('Français','Accueil')
+  })
 
-    //7. Click on Русский on the language menu
-    cy.get('.nav-link').contains('Français').click()
-    cy.get('button').contains('Русский').click().wait(1000)
-    cy.get('.nav-link').eq(0).should('have.text', 'Главная')
+  it('Click on Русский on the language menu', () => {
+    cy.switchLanguage('Русский','Главная')
+  })
 
-    //8. Click on Українська on the language menu
-    cy.get('.nav-link').contains('Русский').click()
-    cy.get('button').contains('Українська').click().wait(1000)
-    cy.get('.nav-link').eq(0).should('have.text', 'Головна')
+  it('Click on Українська on the language menu', () => {
+    cy.switchLanguage('Українська','Головна')
+  })
 
-    //9. Click on English on the language menu
-    cy.get('.nav-link').contains('Українська').click()
-    cy.get('button').contains('English').click().wait(1000)
-    cy.get('.nav-link').eq(0).should('have.text', 'Home')
+  it('Click on English on the language menu', () => {
+    cy.switchLanguage('English','Home')
+  })
 
+  it('Check settings link on account menu', () => {
     //10. Check settings link on account menu
     cy.get('[data-cy=accountMenu]').click()
     cy.get('[data-cy=settings]').click()
     cy.url().should('include', '/account/settings')
     cy.get('[data-cy=settings]').should('have.class', 'active')
+  })
 
+  it('Check password link on account menu', () => {
     //10. Check password link on account menu
     cy.get('[data-cy=accountMenu]').click()
     cy.get('[data-cy=passwordItem]').click()
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,3 +5,10 @@ Cypress.Commands.add('login', (username, password) => {
     cy.get('[data-cy=submit]').click();
     cy.get('#entity-menu').should('be.visible');
   });
+
+  Cypress.Commands.add('switchLanguage', (language, expectedText) => {
+    cy.get('.nav-link').eq(3).click()
+    cy.contains(language).click();
+    cy.get('.nav-link').eq(0).contains(expectedText);
+  });
+
